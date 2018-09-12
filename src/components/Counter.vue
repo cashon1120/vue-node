04996b5f2622ac64e73c88693c25f5d69c909fae
cashon1120 <49798613@qq.com>
2018-09-12 16:39:09 +0800

    add counter.vue

---
+++ src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <button @click="add">+</button>
+        <button @click="reduce">-</button>
+        <p><span>{{number}}</span></p>
+    </div>
+</template>
+<script>
+    export default {
+        props:["number"],
+        data(){
+            return {
+
+            }
+        },
+        methods:{
+            add(){
+                this.$emit("add")
+            },
+            reduce(){
+                this.$emit("reduce")
+            }
+        }
+    }
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,36 @@
 <template>
   <div class="hello">
-  
+    <Counter v-bind:number="number" v-on:add="add" v-on:reduce="reduce"></Counter>
   </div>
 </template>
 
 <script>
+import Counter from "./Counter.vue";
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      number: 10
+    };
+  },
+  methods: {
+    add() {
+      this.number++;
+    },
+    reduce() {
+      this.number--;
     }
+  },
+  components: {
+    Counter
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
