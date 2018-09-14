f9eb60101eb5dc0b6dfa00939eaefa0a96412d2f
cashon1120 <49798613@qq.com>
2018-09-15 06:46:07 +0800

    分离服务端

--- server/models/goods.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema;
-const productSchema = new Schema({
-    "productId": String,
-    "productName": String,
-    "salePrice": Number,
-    "productImage": String
-})
-module.exports = mongoose.model('Good', productSchema)
